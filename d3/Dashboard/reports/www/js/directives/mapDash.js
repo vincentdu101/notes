c8e0f61449ce1ebf7425b67e2ef07beda6d95e39
Vincent Du <vincentdu101@gmail.com>
2015-05-16 21:56:01 -0700

    added us map with initial setup of coloring

--- d3/Dashboard/reports/www/js/directives/mapDash.js
+++ d3/Dashboard/reports/www/js/directives/mapDash.js
@@ -14,8 +14,8 @@ app.directive('mapDash', function(){
       // dimensions of map
       function drawMap(options) {
         var target = options.target;
-        var w = 700;
-        var h = 300;
+        var w = 800;
+        var h = 400;
 
         // scale map to fit
         var projection = d3.geo.albersUsa()
@@ -28,13 +28,66 @@ app.directive('mapDash', function(){
 
         var svg = d3.select('body').select(target).append('svg').attr({width: w, height: h});
 
-        d3.json('js/us.json', function(json){
-          svg.selectAll('path')
-            .data(json.features)
-            .enter()
-            .append('path')
-            .attr('d', path)
-            .attr('fill', '#666666');
+        var color = d3.scale.category10().domain(d3.range(9)),
+            selectedColor = 0,
+            dragColor;
+
+        var components = color.domain().map(function() { return []; });
+
+        d3.json('js/us.json', function(error, us){
+          console.log(us);
+          var features = topojson.feature(us, us.objects.states).features;
+
+          svg.append('path')
+            .datum(topojson.mesh(us, us.objects.states))
+            .attr('class', 'background')
+            .attr('d', path);
+
+          var merge = svg.append('g')
+            .attr('class', 'merge')
+            .selectAll('path')
+            .data(components)
+            .enter().append('path')
+              .style('fill', function(d, i){ return color(i); })
+              .style('stroke', function(d, i){ return d3.lab(color(i)).darker(); });
+
+          svg.append('g')
+            .attr('class', 'foreground')
+            .style('cursor', 'pointer')
+            .style('stroke-capacity', .5)
+            .selectAll('path')
+              .data(features)
+            .enter().append('path')
+              .attr('d', function(d){ d.color = null; return path(d); })
+              .on('mouseover', function(){ this.style.stroke = 'black'; })
+              .on('mouseout', function(){ this.style.stroke = 'none'; })
+              .call(d3.behavior.drag()
+                .on('dragstart', dragstart)
+                .on('drag', drag));
+
+          top.location.hash.split('').slice(1, features.length).forEach(function(c, i){
+            if ((c = +c) >= 0 && c < 10) assign(features[i], c ? c - 1: null);
+          });
+
+          redraw();
+
+          function dragstart() {
+            var feature = d3.event.sourceEvent.target.__data__;
+             dragColor = feature.color === selectedColor ? null : selectedColor;
+            if (assign(feature, dragColor)) {
+              redraw();
+            }
+          }
+
+          function drag() {
+            var feature = d3.event.sourceEvent.target.__data__;
+            if (feature && assign(feature, dragColor)) {
+              redraw();
+            }
+          }
+
+          function 
+
         });
       }
       drawMap({target: $scope.target});
