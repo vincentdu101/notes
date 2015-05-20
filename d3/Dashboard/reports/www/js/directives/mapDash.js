ff8f29254fe09c419ac300e9a1282237f4f3e924
Vincent Du <vincentdu101@gmail.com>
2015-05-19 23:08:49 -0700

    added the ability to reset color and colored map by default

--- d3/Dashboard/reports/www/js/directives/mapDash.js
+++ d3/Dashboard/reports/www/js/directives/mapDash.js
@@ -11,6 +11,16 @@ app.directive('mapDash', function(){
 
       $scope.target = $scope.target ? $scope.target : ".map-dash-target";
 
+      function useFocusColor(feature) {
+        if (feature) {
+          d3.selectAll('path')
+            .classed('selected', function(d, i){
+              return d.id !== feature.id;
+            });
+        }
+      }
+
+
       // dimensions of map
       function drawMap(options) {
         var target = options.target;
@@ -22,20 +32,21 @@ app.directive('mapDash', function(){
                           .translate([w/2, h/2])
 
                           // max number for determining scale ratio
-                          .scale([500]);
+                          .scale([750]);
 
         var path = d3.geo.path().projection(projection);
 
         var svg = d3.select('body').select(target).append('svg').attr({width: w, height: h});
 
         var color = d3.scale.category10().domain(d3.range(9)),
-            selectedColor = 0,
-            dragColor;
+            selectedColor = 2,
+            dragColor = 1;
 
         var components = color.domain().map(function() { return []; });
 
         d3.json('js/us.json', function(error, us){
-          console.log(us);
+          
+          var bisectId = d3.bisector(function(d){ return d.id; }).left;
           var features = topojson.feature(us, us.objects.states).features;
 
           svg.append('path')
@@ -58,22 +69,25 @@ app.directive('mapDash', function(){
             .selectAll('path')
               .data(features)
             .enter().append('path')
-              .attr('d', function(d){ d.color = null; return path(d); })
+              .attr('d', function(d, i){ d.color = null; return path(d); })
+              .attr('class', 'selected')
+              .attr('data-id', function(d, i){ return d.id; })
               .on('mouseover', function(){ this.style.stroke = 'black'; })
               .on('mouseout', function(){ this.style.stroke = 'none'; })
               .call(d3.behavior.drag()
                 .on('dragstart', dragstart)
                 .on('drag', drag));
 
-          top.location.hash.split('').slice(1, features.length).forEach(function(c, i){
-            if ((c = +c) >= 0 && c < 10) assign(features[i], c ? c - 1: null);
-          });
+          // top.location.hash.split('').slice(1, features.length).forEach(function(c, i){
+          //   if ((c = +c) >= 0 && c < 10) assign(features[i], c ? c - 1: null);
+          // });
 
           redraw();
 
           function dragstart() {
             var feature = d3.event.sourceEvent.target.__data__;
-             dragColor = feature.color === selectedColor ? null : selectedColor;
+            useFocusColor(feature);
+            dragColor = feature.color === selectedColor ? 0 : selectedColor;
             if (assign(feature, dragColor)) {
               redraw();
             }
@@ -81,12 +95,32 @@ app.directive('mapDash', function(){
 
           function drag() {
             var feature = d3.event.sourceEvent.target.__data__;
+            useFocusColor(feature);
             if (feature && assign(feature, dragColor)) {
               redraw();
             }
           }
 
-          function 
+          function assign(feature, color) {
+            if (feature.color !== null) {
+              var component = components[feature.color];
+              component.splice(bisectId(component, feature.id), 0, feature);
+              feature.color = color;
+            }
+            if (color !== null) {
+              var component = components[color];
+              component.splice(bisectId(component, feature.id), 0, feature);
+              feature.color = color;
+            }
+            return true;
+          }
+
+          function redraw() {
+            merge.data(components).attr('d', function(d){ 
+              return path({type: 'FeatureCollection', features: d}) || "M0,0"; });
+            // stores colors history in url, not needed but interesting reference
+            // top.history.replaceState(null, null, "#" + features.map(function(d){ return d.color === null ? "0": d.color + 1; }).join(""));
+          }
 
         });
       }
